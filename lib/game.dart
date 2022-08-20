c8b62e34d5d7746caea24b2d88d12ee8c67845e3
Thanakit <thanakit908@gmail.com>
2022-08-20 15:53:39 +0700

    test edit agian

--- lib/game.dart
+++ lib/game.dart
@@ -10,7 +10,7 @@ class Game{
   }
   int doGuess(int num){
     if(num > answer){
-      print('$num is too high999999');
+      print('$num is too high9');
       return 0;
     }
     else if(num < answer){
